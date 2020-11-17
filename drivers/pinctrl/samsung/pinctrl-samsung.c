08883d81515ff3cfa0d1b60b78b93bbb31954d2a
Ivaylo Ivanov <ivo.ivanov@null.net>
2020-11-17 15:07:41 +0200

    pinctrl: samsung: Add support for exynos8895
    This patch adds support for Exynos 8895 Soc in the
    existing arm64 exynos pinctrl driver.
    
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov@null.net>

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -1264,6 +1264,8 @@ static const struct of_device_id samsung_pinctrl_dt_match[] = {
 		.data = &exynos5433_of_data },
 	{ .compatible = "samsung,exynos7-pinctrl",
 		.data = &exynos7_of_data },
+	{ .compatible = "samsung,exynos8895-pinctrl",
+                .data = &exynos8895_of_data },
 #endif
 #ifdef CONFIG_PINCTRL_S3C64XX
 	{ .compatible = "samsung,s3c64xx-pinctrl",
