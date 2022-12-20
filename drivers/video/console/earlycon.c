d35f86ab8f851af94366994bb04c76f5c1ea5a1b
Markuss Broks <markuss.broks@gmail.com>
2022-12-20 22:24:37 +0200

    efi: earlycon: Add support for generic framebuffers
    Add early console support for generic linear framebuffer devices.
    This driver supports probing from cmdline early parameters
    or from the device-tree using information in simple-framebuffer node.
    The EFI functionality should be retained in whole.
    The driver was disabled on ARM because of a bug in early_ioremap
    implementation on ARM and on IA64 because of lack of early_memremap_prot.
    
    Signed-off-by: Markuss Broks <markuss.broks@gmail.com>

--- drivers/video/console/earlycon.c
+++ drivers/video/console/earlycon.c
@@ -1,118 +1,122 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2013 Intel Corporation; author Matt Fleming
+ * Copyright (C) 2022 Markuss Broks <markuss.broks@gmail.com>
  */
 
+#include <asm/early_ioremap.h>
 #include <linux/console.h>
 #include <linux/efi.h>
 #include <linux/font.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/of.h>
+#include <linux/of_fdt.h>
 #include <linux/serial_core.h>
 #include <linux/screen_info.h>
 
-#include <asm/early_ioremap.h>
+struct fb_earlycon {
+	u32 x, y, curr_x, curr_y, depth, stride;
+	size_t size;
+	phys_addr_t phys_base;
+	void __iomem *virt_base;
+};
 
-static const struct console *earlycon_console __initdata;
+static const struct console *earlycon_console __initconst;
+static struct fb_earlycon info;
 static const struct font_desc *font;
-static u32 efi_x, efi_y;
-static u64 fb_base;
-static bool fb_wb;
-static void *efi_fb;
 
-/*
- * EFI earlycon needs to use early_memremap() to map the framebuffer.
- * But early_memremap() is not usable for 'earlycon=efifb keep_bootcon',
- * memremap() should be used instead. memremap() will be available after
- * paging_init() which is earlier than initcall callbacks. Thus adding this
- * early initcall function early_efi_map_fb() to map the whole EFI framebuffer.
- */
-static int __init efi_earlycon_remap_fb(void)
+static int __init simplefb_earlycon_remap_fb(void)
 {
-	/* bail if there is no bootconsole or it was unregistered already */
-	if (!earlycon_console || !console_is_registered(earlycon_console))
+	unsigned long mapping;
+	/* bail if there is no bootconsole or it has been disabled already */
+	if (!earlycon_console || !(earlycon_console->flags & CON_ENABLED))
 		return 0;
 
-	efi_fb = memremap(fb_base, screen_info.lfb_size,
-			  fb_wb ? MEMREMAP_WB : MEMREMAP_WC);
+	if (region_intersects(info.phys_base, info.size,
+			      IORESOURCE_SYSTEM_RAM, IORES_DESC_NONE) == REGION_INTERSECTS)
+		mapping = MEMREMAP_WB;
+	else
+		mapping = MEMREMAP_WC;
 
-	return efi_fb ? 0 : -ENOMEM;
+	info.virt_base = memremap(info.phys_base, info.size, mapping);
+
+	return info.virt_base ? 0 : -ENOMEM;
 }
-early_initcall(efi_earlycon_remap_fb);
+early_initcall(simplefb_earlycon_remap_fb);
 
-static int __init efi_earlycon_unmap_fb(void)
+static int __init simplefb_earlycon_unmap_fb(void)
 {
-	/* unmap the bootconsole fb unless keep_bootcon left it registered */
-	if (efi_fb && !console_is_registered(earlycon_console))
-		memunmap(efi_fb);
+	/* unmap the bootconsole fb unless keep_bootcon has left it enabled */
+	if (info.virt_base && !(earlycon_console->flags & CON_ENABLED))
+		memunmap(info.virt_base);
 	return 0;
 }
-late_initcall(efi_earlycon_unmap_fb);
+late_initcall(simplefb_earlycon_unmap_fb);
 
-static __ref void *efi_earlycon_map(unsigned long start, unsigned long len)
+static __ref void *simplefb_earlycon_map(unsigned long start, unsigned long len)
 {
 	pgprot_t fb_prot;
 
-	if (efi_fb)
-		return efi_fb + start;
+	if (info.virt_base)
+		return info.virt_base + start;
 
-	fb_prot = fb_wb ? PAGE_KERNEL : pgprot_writecombine(PAGE_KERNEL);
-	return early_memremap_prot(fb_base + start, len, pgprot_val(fb_prot));
+	fb_prot = PAGE_KERNEL;
+	return early_memremap_prot(info.phys_base + start, len, pgprot_val(fb_prot));
 }
 
-static __ref void efi_earlycon_unmap(void *addr, unsigned long len)
+static __ref void simplefb_earlycon_unmap(void *addr, unsigned long len)
 {
-	if (efi_fb)
+	if (info.virt_base)
 		return;
 
 	early_memunmap(addr, len);
 }
 
-static void efi_earlycon_clear_scanline(unsigned int y)
+static void simplefb_earlycon_clear_scanline(unsigned int y)
 {
 	unsigned long *dst;
 	u16 len;
 
-	len = screen_info.lfb_linelength;
-	dst = efi_earlycon_map(y*len, len);
+	len = info.stride;
+	dst = simplefb_earlycon_map(y * len, len);
 	if (!dst)
 		return;
 
 	memset(dst, 0, len);
-	efi_earlycon_unmap(dst, len);
+	simplefb_earlycon_unmap(dst, len);
 }
 
-static void efi_earlycon_scroll_up(void)
+static void simplefb_earlycon_scroll_up(void)
 {
 	unsigned long *dst, *src;
 	u16 len;
 	u32 i, height;
 
-	len = screen_info.lfb_linelength;
-	height = screen_info.lfb_height;
+	len = info.stride;
+	height = info.y;
 
 	for (i = 0; i < height - font->height; i++) {
-		dst = efi_earlycon_map(i*len, len);
+		dst = simplefb_earlycon_map(i * len, len);
 		if (!dst)
 			return;
 
-		src = efi_earlycon_map((i + font->height) * len, len);
+		src = simplefb_earlycon_map((i + font->height) * len, len);
 		if (!src) {
-			efi_earlycon_unmap(dst, len);
+			simplefb_earlycon_unmap(dst, len);
 			return;
 		}
 
 		memmove(dst, src, len);
 
-		efi_earlycon_unmap(src, len);
-		efi_earlycon_unmap(dst, len);
+		simplefb_earlycon_unmap(src, len);
+		simplefb_earlycon_unmap(dst, len);
 	}
 }
 
-static void efi_earlycon_write_char(u32 *dst, unsigned char c, unsigned int h)
+static void simplefb_earlycon_write_char(u8 *dst, unsigned char c, unsigned int h)
 {
-	const u32 color_black = 0x00000000;
-	const u32 color_white = 0x00ffffff;
 	const u8 *src;
 	int m, n, bytes;
 	u8 x;
@@ -124,23 +128,21 @@ static void efi_earlycon_write_char(u32 *dst, unsigned char c, unsigned int h)
 		n = m % 8;
 		x = *(src + m / 8);
 		if ((x >> (7 - n)) & 1)
-			*dst = color_white;
+			memset(dst, 0xff, (info.depth / 8));
 		else
-			*dst = color_black;
-		dst++;
+			memset(dst, 0, (info.depth / 8));
+		dst += (info.depth / 8);
 	}
 }
 
 static void
-efi_earlycon_write(struct console *con, const char *str, unsigned int num)
+simplefb_earlycon_write(struct console *con, const char *str, unsigned int num)
 {
-	struct screen_info *si;
 	unsigned int len;
 	const char *s;
 	void *dst;
 
-	si = &screen_info;
-	len = si->lfb_linelength;
+	len = info.stride;
 
 	while (num) {
 		unsigned int linemax;
@@ -152,95 +154,152 @@ efi_earlycon_write(struct console *con, const char *str, unsigned int num)
 			count++;
 		}
 
-		linemax = (si->lfb_width - efi_x) / font->width;
+		linemax = (info.x - info.curr_x) / font->width;
 		if (count > linemax)
 			count = linemax;
 
 		for (h = 0; h < font->height; h++) {
 			unsigned int n, x;
 
-			dst = efi_earlycon_map((efi_y + h) * len, len);
+			dst = simplefb_earlycon_map((info.curr_y + h) * len, len);
 			if (!dst)
 				return;
 
 			s = str;
 			n = count;
-			x = efi_x;
+			x = info.curr_x;
 
 			while (n-- > 0) {
-				efi_earlycon_write_char(dst + x*4, *s, h);
+				simplefb_earlycon_write_char(dst + (x * 4), *s, h);
 				x += font->width;
 				s++;
 			}
 
-			efi_earlycon_unmap(dst, len);
+			simplefb_earlycon_unmap(dst, len);
 		}
 
 		num -= count;
-		efi_x += count * font->width;
+		info.curr_x += count * font->width;
 		str += count;
 
 		if (num > 0 && *s == '\n') {
-			efi_x = 0;
-			efi_y += font->height;
+			info.curr_x = 0;
+			info.curr_y += font->height;
 			str++;
 			num--;
 		}
 
-		if (efi_x + font->width > si->lfb_width) {
-			efi_x = 0;
-			efi_y += font->height;
+		if (info.curr_x + font->width > info.x) {
+			info.curr_x = 0;
+			info.curr_y += font->height;
 		}
 
-		if (efi_y + font->height > si->lfb_height) {
+		if (info.curr_y + font->height > info.y) {
 			u32 i;
 
-			efi_y -= font->height;
-			efi_earlycon_scroll_up();
+			info.curr_y -= font->height;
+			simplefb_earlycon_scroll_up();
 
 			for (i = 0; i < font->height; i++)
-				efi_earlycon_clear_scanline(efi_y + i);
+				simplefb_earlycon_clear_scanline(info.curr_y + i);
 		}
 	}
 }
 
-static int __init efi_earlycon_setup(struct earlycon_device *device,
-				     const char *opt)
+static int __init simplefb_earlycon_setup_common(struct earlycon_device *device,
+						 const char *opt)
 {
-	struct screen_info *si;
-	u16 xres, yres;
-	u32 i;
+	int i;
 
-	if (screen_info.orig_video_isVGA != VIDEO_TYPE_EFI)
-		return -ENODEV;
+	info.size = info.x * info.y * (info.depth / 8);
 
-	fb_base = screen_info.lfb_base;
-	if (screen_info.capabilities & VIDEO_CAPABILITY_64BIT_BASE)
-		fb_base |= (u64)screen_info.ext_lfb_base << 32;
-
-	fb_wb = opt && !strcmp(opt, "ram");
-
-	si = &screen_info;
-	xres = si->lfb_width;
-	yres = si->lfb_height;
-
-	/*
-	 * efi_earlycon_write_char() implicitly assumes a framebuffer with
-	 * 32 bits per pixel.
-	 */
-	if (si->lfb_depth != 32)
-		return -ENODEV;
-
-	font = get_default_font(xres, yres, -1, -1);
+	font = get_default_font(info.x, info.y, -1, -1);
 	if (!font)
 		return -ENODEV;
 
-	efi_y = rounddown(yres, font->height) - font->height;
-	for (i = 0; i < (yres - efi_y) / font->height; i++)
-		efi_earlycon_scroll_up();
+	info.curr_y = rounddown(info.y, font->height) - font->height;
+	for (i = 0; i < (info.y - info.curr_y) / font->height; i++)
+		simplefb_earlycon_scroll_up();
 
-	device->con->write = efi_earlycon_write;
+	device->con->write = simplefb_earlycon_write;
 	earlycon_console = device->con;
 	return 0;
 }
-EARLYCON_DECLARE(efifb, efi_earlycon_setup);
+
+static int __init simplefb_earlycon_setup(struct earlycon_device *device,
+					  const char *opt)
+{
+	struct uart_port *port = &device->port;
+	int ret;
+
+	if (!port->mapbase)
+		return -ENODEV;
+
+	info.phys_base = port->mapbase;
+
+	ret = sscanf(device->options, "%ux%ux%u", &info.x, &info.y, &info.depth);
+	if (ret != 3)
+		return -ENODEV;
+
+	info.stride = info.x * (info.depth / 8);
+
+	return simplefb_earlycon_setup_common(device, opt);
+}
+
+EARLYCON_DECLARE(simplefb, simplefb_earlycon_setup);
+
+#ifdef CONFIG_EFI_EARLYCON
+static int __init simplefb_earlycon_setup_efi(struct earlycon_device *device,
+					      const char *opt)
+{
+	if (screen_info.orig_video_isVGA != VIDEO_TYPE_EFI)
+		return -ENODEV;
+
+	info.phys_base = screen_info.lfb_base;
+	if (screen_info.capabilities & VIDEO_CAPABILITY_64BIT_BASE)
+		info.phys_base |= (u64)screen_info.ext_lfb_base << 32;
+
+	info.x = screen_info.lfb_width;
+	info.y = screen_info.lfb_height;
+	info.depth = screen_info.lfb_depth;
+	info.stride = screen_info.lfb_linelength;
+
+	return simplefb_earlycon_setup_common(device, opt);
+}
+
+EARLYCON_DECLARE(efifb, simplefb_earlycon_setup_efi);
+#endif
+
+#ifdef CONFIG_OF_EARLY_FLATTREE
+static int __init simplefb_earlycon_setup_of(struct earlycon_device *device,
+					     const char *opt)
+{
+	struct uart_port *port = &device->port;
+	const __be32 *val;
+
+	if (!port->mapbase)
+		return -ENODEV;
+
+	info.phys_base = port->mapbase;
+
+	val = of_get_flat_dt_prop(device->node, "width", NULL);
+	if (!val)
+		return -ENODEV;
+	info.x = be32_to_cpu(*val);
+
+	val = of_get_flat_dt_prop(device->node, "height", NULL);
+	if (!val)
+		return -ENODEV;
+	info.y = be32_to_cpu(*val);
+
+	val = of_get_flat_dt_prop(device->node, "stride", NULL);
+	if (!val)
+		return -ENODEV;
+	info.stride = be32_to_cpu(*val);
+	info.depth = (info.stride / info.x) * 8;
+
+	return simplefb_earlycon_setup_common(device, opt);
+}
+
+OF_EARLYCON_DECLARE(simplefb, "simple-framebuffer", simplefb_earlycon_setup_of);
+#endif
